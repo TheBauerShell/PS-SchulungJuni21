b43e2480645be3525ea0ac21acc49689e8c7a9c1
Peter Monadjemi <pm@activetraining.de>
2021-06-23 19:05:59 +0200

    Weitere Advanced Function-Beispiele

---
+++ Tag1/Bsp8_ShouldSupports_Bsp1.ps1
@@ -0,0 +1,17 @@
+<#
+ .Synopsis
+ Beispiel für SupportsShouldProcess mit Remove-Item
+ .Description
+ Durch SupportShouldProcess erhält die Function einen Confirm-Parameter, der automatisch an die beiden Cmdlets weitergereicht wird
+ Frage: Wie oft muss die Ausführung der Function bestätigt werden?
+#>
+
+function Test-Confirm
+{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    param()
+    New-Item -Name Test.dat -ItemType File | Out-Null
+    "Test.dat" | Remove-item
+}
+
+Test-Confirm -Confirm
